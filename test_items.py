f3a7ec6013e3240348e001a53fc7d09a33f3cef3
Igor Orlov <igororlov2205@gmail.com>
2022-06-26 15:40:11 +0300

    homework for lesson 3.6.9

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_language_transmission(browser):
+    browser.get(link)
+    add_to_basket = browser.find_element(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert add_to_basket
+    add_to_basket.click()
+    time.sleep(10)
